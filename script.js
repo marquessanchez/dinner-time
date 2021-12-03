f887444408c8a3765868dcfa4025d186bd664b20
Sean Gillespie <seene3@gmail.com>
2021-12-02 21:08:18 -0500

    added vars currently a little broken

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
 const mealDBEndpoint = "https://www.themealdb.com/api/json/v1/1/random.php"
 const cocktailDBEndpoint = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+const meal = Document.getElementById("meal")
+const drink = Document.getElementById("drink")
 
-var getMeal = async () => {
+var mealName = ""
+var mealThumb = ""
+var drinkName = ""
+var drinkThumb = ""
+
+
+//get meal name and thumbnail
+var pickMeal = async () => {
     const res = await fetch(mealDBEndpoint)
     let data = await res.json()
+    mealName = data.meals[0].strMeal
+    mealThumb = data.meals[0].strMealThumb
 
+    console.log(data)
     console.log(data.meals[0].strMeal)
     console.log(data.meals[0].strMealThumb)
+
+    
 }
 
-var getDrink = async () => {
+
+//get drink name and thumbnail
+var pickDrink = async () => {
     const res = await fetch(cocktailDBEndpoint)
     let data = await res.json()
-
+    drinkName = data.drinks[0].strDrink
+    drinkThumb = data.drinks[0].strDrinkThumb
+    console.log(data)
     console.log(data.drinks[0].strDrink)
     console.log(data.drinks[0].strDrinkThumb)
 }
 
-getDrink()
-getMeal()
+
+pickDrink()
+pickMeal()
