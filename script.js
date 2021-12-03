2b8158fb1526d2ee1737b9e4a8618e95ff7b6aef
Sean Gillespie <seene3@gmail.com>
2021-12-02 20:22:02 -0500

    fixed apis

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-const mealDBEndpoint = "www.themealdb.com/api/json/v1/1/random.php"
-const cocktailDBEndpoint = "www.thecocktaildb.com/api/json/v1/1/random.php"
+const mealDBEndpoint = "https://www.themealdb.com/api/json/v1/1/random.php"
+const cocktailDBEndpoint = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
 
 var getMeal = async () => {
     const res = await fetch(mealDBEndpoint)
     let data = await res.json()
 
-    console.log(data)
+    console.log(data.meals[0].strMeal)
+    console.log(data.meals[0].strMealThumb)
 }
 
+var getDrink = async () => {
+    const res = await fetch(cocktailDBEndpoint)
+    let data = await res.json()
+
+    console.log(data.drinks[0].strDrink)
+    console.log(data.drinks[0].strDrinkThumb)
+}
+
+getDrink()
 getMeal()
